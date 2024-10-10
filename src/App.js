9e87d31064a2a5386ed753ba16ced47eb66e611a
Evan McKinney-Robinson <evan.mckinney-robinson@theverygroup.com>
2024-10-10 08:03:24 +0100

    Create Books component to map out JSON data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { BooksProvider } from './BooksContext';
+import Books from './components/Books';
 
 function App() {
   return (
     <BooksProvider>
-      <div className="App"></div>
+      <div className="App">
+        <Books>
+        </Books> 
+      </div>
     </BooksProvider>
   );
 }
--- src/BooksContext.js
+++ src/BooksContext.js
@@ -3,14 +3,18 @@ import React, {useState, useEffect, createContext} from 'react';
 export const BooksContext = createContext();
 
 export const BooksProvider = ({ children }) => {
+
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-            .then(data => setBooks(data))
-            .catch(error => console.error(error));
+        fetch('/books.json')
+        .then(response => response.json())
+        .then(data => {
+            setBooks(data)
+            setLoading(false)
+        })
+        .catch(error => console.error(error))
     }, []);
 
     return (
